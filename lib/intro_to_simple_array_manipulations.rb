830650d04718dc2067e5f23e18726e6e674e3f34
shmuly levitin <shmuly11@gmail.com>
2020-11-17 17:49:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,15 @@ end
 def using_uniq(array)
   array.uniq 
 end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def usind_delete_at(array, interger)
+  array.delete_at(interger)
+end
